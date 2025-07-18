64fdbe7ec627a1d41a7cba79275564cbc1b7d808
jbrown-xentity <jbrown@xentity.com>
2025-07-18 16:11:57 -0600

    Update to allow for testing in folder

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
 # test_main.py
 
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from unittest.mock import patch
 from io import StringIO
 
