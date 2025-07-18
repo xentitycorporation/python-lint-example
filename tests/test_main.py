c1c6c68045d00489c7339d25ef558f3a1b87bb8d
jbrown-xentity <jbrown@xentity.com>
2025-07-18 16:16:52 -0600

    Fix with isort

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
 # test_main.py
 
-import sys
 import os
+import sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from main import encode, decode
+from main import decode, encode
 
 
 def test_encode():
