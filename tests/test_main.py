08b81bed5d7027a4785924be3132a3d5262fcfc1
jbrown-xentity <jbrown@xentity.com>
2025-07-18 16:15:50 -0600

    Utilize black

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,15 @@
 
 import sys
 import os
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import encode, decode
 
+
 def test_encode():
-    assert encode('hey') == 'khB'
+    assert encode("hey") == "khB"
+
 
 def test_decode():
-    assert decode('khB') == 'hey'
+    assert decode("khB") == "hey"
